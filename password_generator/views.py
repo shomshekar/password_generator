69dbbd2396458c15f94744c5fd4c1cc30eed9b91
Shomshekar Shilvant <shomshekar@rediff.com>
2024-01-31 18:38:30 +0530

    changes for special character

--- password_generator/views.py
+++ password_generator/views.py
@@ -50,7 +50,7 @@ def generate_characters(request):
     if 'numbers' in request.POST.keys():
         characterList += string.digits
     if 'specialCharacter' in request.POST.keys():
-        characterList += '!#$%&;()*+-:;&lt;=&gt;?@_|'
+        characterList += '!#$%&()*+-<=>?@_|'
     
     return characterList
 
