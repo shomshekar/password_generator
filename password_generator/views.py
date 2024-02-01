ffbe8e47d7d217e13277b0d1c6e28aef17e341ad
Shomshekar Shilvant <shomshekar@rediff.com>
2024-02-01 12:24:39 +0530

    adding additional conditions for generating password

--- password_generator/views.py
+++ password_generator/views.py
@@ -50,9 +50,27 @@ def generate_characters(request):
     if 'numbers' in request.POST.keys():
         characterList += string.digits
     if 'specialCharacter' in request.POST.keys():
-        characterList += '!#$%&()*+-<=>?@_|'
+        characterList += '!#$%&()*+-:;<=>?@_|'
     
     return characterList
+
+
+def checkPasswordConditions(request, context):
+
+    password_length = request.POST['passwordLength']
+    if not password_length.isdigit():
+        context['error_msg'] = "Password length should be of Type Integer from 6 to 20"
+    else:
+        password_length = int(password_length)
+        if password_length < 6 or password_length > 20:
+            context['error_msg'] = "Password length should be from 6 to 20"
+        else:
+            if ( 'upperCase' not in request.POST and 'lowerCase' not in request.POST and 'numbers' not in request.POST and 'specialCharacter' not in request.POST ):
+                context['error_msg'] = "Atleast one of the checkbox like uppercase or lowercase need to be selected" 
+            elif ( 'upperCase' not in request.POST and 'lowerCase' not in request.POST and 'specialCharacter' not in request.POST and 'numbers' in request.POST and 'noRepeatCharacter' in request.POST and password_length > len(string.digits) ):
+                context['error_msg'] = "Cannot generate password without repeating to match password length"
+
+    return context
 
 
 def home(request):
@@ -64,36 +82,24 @@ def home(request):
 
     # Check to see if we are getting a POST request back
     if request.method == "POST":
-        if 'passwordLength' not in request.POST:
-            context['error_msg'] = "Password length is required"
-        elif ( 'upperCase' not in request.POST and 'lowerCase' not in request.POST and 'numbers' not in request.POST and 'specialCharacter' not in request.POST ):
-            context['error_msg'] = "Atleast one of the checkbox like uppercase or lowercase need to be selected" 
-        
-        else:
 
-            form = forms.FormName(request.POST)
+        form = forms.FormName(request.POST)
+        password_length = 0
+        checkPasswordConditions(request, context)
+
+        if 'error_msg' not in context:
             # Then we check to see if the form is valid
             if form.is_valid():
-                password_length = form.cleaned_data['passwordLength']
-                if password_length.isdigit():
-                    password_length = int(password_length)
-                    if password_length >=6 and password_length <=20:
+                password_length = int(password_length)
+                characterList = generate_characters(request)
 
-                        characterList = generate_characters(request)
-                        no_repeat = True if 'noRepeatCharacter' in request.POST.keys() else False
+                no_repeat = True if 'noRepeatCharacter' in request.POST.keys() else False
+                password = generate_password(characterList, password_length, no_repeat)
 
-                        password = generate_password(characterList, password_length, no_repeat)
-
-                        context['password'] = password
-                        # To check if password generated is strong or not
-                        context['message'] = checkPassword(password)
-                    else:
-                        context['error_msg'] = "Password length should be from 6 to 20"
-                else:
-                    context['error_msg'] = "Password length should be of Type Integer from 6 to 20"
+                context['password'] = password
+                # To check if password generated is strong or not
+                context['message'] = checkPassword(password)
             else:
                 context['message'] = form.errors
-            
-
 
     return HttpResponse(template.render(context, request))
