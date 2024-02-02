5677a55adfdef8c7527278c8acdc30df818289c9
Shomshekar Shilvant <shomshekar@rediff.com>
2024-02-02 12:03:27 +0530

    minor changes to allow css urls on server

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
